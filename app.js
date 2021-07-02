6482afa3942b3b41605978bac2ddc61ec6741f58
jalessandrog <a01701434@itesm.mx>
2021-07-02 10:21:30 -0500

    3 commit

--- app.js
+++ app.js
@@ -3,20 +3,16 @@ const app = express();
 
 app.use('/static', express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
     console.log('Servidor funcionando en ' + PORT)
 })
 
-app.get('/', (req, res) => {
-    res.json({ success: true })
-})
-/*
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/home.html');
-})
-*/
 app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 })
