48ec3474586909c604ae531ba1c3e04ddc8b6614
jalessandrog <a01701434@itesm.mx>
2021-07-02 10:04:39 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+
+app.use('/static', express.static('public'));
+
+const PORT =process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+    console.log('Servidor funcionando en ' + PORT)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+})
+
